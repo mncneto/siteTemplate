c7a785824c2d9cc6a9c239ccb7a95182469f8abf
Manoel <manoelncneto@gmail.com>
2018-07-07 18:43:16 -0300

    primeiro Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron" style="background:#87CEEB" tabindex="-1">
+        <div class="container"> 
+            <h1>Cabeçalho da Página</h1> 
+            <p>Breve descrição da pagina</p> 
+        </div>
+    </div>
+@endsection
